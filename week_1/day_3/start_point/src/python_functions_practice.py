039567fc7bfa30c2feeafd9a8485c5eed0cfa7f9
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2022-11-10 09:15:04 +0000

    day_3 homework

---
+++ week_1/day_3/start_point/src/python_functions_practice.py
@@ -0,0 +1,105 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(num_1, num_2):
+    return int(num_1) + int(num_2)
+
+def number_to_full_month_name(month):
+    if month == 1:
+        return "January"
+    if month == 2:
+        return "February"
+    if month == 3:
+        return "March"
+    if month == 4:
+        return "April"
+    if month == 5:
+        return "May"
+    if month == 6:
+        return "June"
+    if month == 7:
+        return "July"
+    if month == 8:
+        return "August"
+    if month == 9:
+        return "September"
+    if month == 10:
+        return "October"
+    if month == 11:
+        return "November"
+    if month == 12:
+        return "December"
+
+    
+def number_to_full_month_name(month):
+    if month == 1:
+        return "January"
+    if month == 2:
+        return "February"
+    if month == 3:
+        return "March"
+    if month == 4:
+        return "April"
+    if month == 5:
+        return "May"
+    if month == 6:
+        return "June"
+    if month == 7:
+        return "July"
+    if month == 8:
+        return "August"
+    if month == 9:
+        return "September"
+    if month == 10:
+        return "October"
+    if month == 11:
+        return "November"
+    if month == 12:
+        return "December"
+
+
+def number_to_short_month_name__month_1(month):
+    d={1:'January', 2: 'Feb', 3: 'March', 4: 'April':, 5: 'May': 6: 'June', 7: 'July', 8:'August', 9: 'Sept', 10: 'October', 11: 'November', 12: 'December'}
+return d(month)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
